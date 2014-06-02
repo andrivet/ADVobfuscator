8d2f643653544bb50d6f514a00cfb4cb5b8ad2ac
Sebastien <sebastien@Pegasus15.local>
2014-06-02 10:51:09 +0200

    Initial Commit

---
+++ ADVobfuscator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ADVobfuscator
+//
+//  Created by Sebastien on 02/06/14.
+//  Copyright (c) 2014 Andrivet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
