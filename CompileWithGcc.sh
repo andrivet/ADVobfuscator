b1c888004280fb5a14c1c3599a313331cd05620e
Sebastien <sebastien@andrivet.com>
2015-08-19 12:59:21 +0200

    Replace shell script by a Makefile to compile with GCC

--- CompileWithGcc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# IMPORTANT: You need to install Boost (www.boost.org) to be able to compile this
-# with command such as ((Debian & similar): 
-#
-# sudo apt-get install libboost-all-dev
-
-g++ -std=c++11 -O3 ADVobfuscator/main.cpp ADVobfuscator/DetectDebugger.cpp -o Sample
